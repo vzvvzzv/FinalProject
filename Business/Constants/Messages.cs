9270ff7a561817c4f3f0bd8acd0df1e0cd091687
vzvvzzv <denizdogan@live.com>
2021-02-10 23:17:53 -0500

    10. Gun Sonu

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        //degisken olmasina ragmen buyuk harfli yazdik cunku public, private olsaydi productAdded olurdu.
+        public static string ProductAdded = "Urun eklendi.";
+        public static string ProductNameInvalid = "Urun ismi gecersiz";
+        public static string MaintenanceTime = "Sistem bakimda.";
+        public static string ProductsListed = "Urunler listelendi.";
+    }
+}

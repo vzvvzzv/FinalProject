21795f0d244245ced49dd4c4b72d18db4dbe7f4b
vzvvzzv <denizdogan@live.com>
2021-02-14 01:45:22 -0500

    13 Ocak Ders Sonu

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -55,7 +55,7 @@ namespace Business.Concrete
 
         public IDataResult<List<ProductDetailDto>> GetProductDetails()
         {
-            if (DateTime.Now.Hour == 22)
+            if (DateTime.Now.Hour == 1)
             {
                 return new ErrorDataResult<List<ProductDetailDto>>(Messages.MaintenanceTime);
             }
